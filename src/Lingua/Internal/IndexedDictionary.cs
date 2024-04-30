c7fde324381d67e19ebb2738c4bfb5e1353917f1
Russ Cam <russ.cam@forloop.co.uk>
2024-04-30 14:08:42 +1000

    tidy up IndexedDictionary

--- src/Lingua/Internal/IndexedDictionary.cs
+++ src/Lingua/Internal/IndexedDictionary.cs
@@ -9,7 +9,7 @@ namespace Lingua.Internal;
 /// </summary>
 /// <typeparam name="TKey">Key type</typeparam>
 /// <typeparam name="TValue">Value type</typeparam>
-public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquatable<IndexedDictionary<TKey, TValue>> where TKey : notnull
+internal sealed class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquatable<IndexedDictionary<TKey, TValue>> where TKey : notnull
 {
     private ValueCollection? _values;
     private readonly Dictionary<TKey, TValue> _dict;
@@ -24,60 +24,10 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
         _dict = new Dictionary<TKey, TValue>();
         _ordering = new List<TKey>();
     }
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="IndexedDictionary{TKey,TValue}"/>
-    ///     that contains elements copied from the specified System.Collections.Generic.IDictionary`2
-    ///     and uses the default equality comparer for the key type.
-    /// </summary>
-    /// <param name="dictionary">The System.Collections.Generic.IDictionary`2 whose elements are copied to the new SquidLib.SquidMath.IndexedDictionary.</param>
-    public IndexedDictionary(IDictionary<TKey, TValue> dictionary) {
-        _dict = new Dictionary<TKey, TValue>(dictionary);
-        _ordering = new List<TKey>(_dict.Keys);
-    }
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="IndexedDictionary{TKey,TValue}"/>
-    ///     that is empty, has the default initial capacity, and uses the specified System.Collections.Generic.IEqualityComparer`1.
-    /// </summary>
-    /// <param name="comparer">The System.Collections.Generic.IEqualityComparer`1 implementation to use when comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1 for the type of the key.</param>
-    public IndexedDictionary(IEqualityComparer<TKey> comparer) {
-        _dict = new Dictionary<TKey, TValue>(comparer);
-        _ordering = new List<TKey>();
-    }
-    /// <summary>
-    /// Initializes a new instance of the <see cref="IndexedDictionary{TKey,TValue}"/>
-    /// that is empty, has the specified initial capacity, and uses the default equality
-    /// comparer for the key type.
-    /// </summary>
-    /// <param name="capacity">The initial number of elements that the SquidLib.SquidMath.IndexedDictionary can contain.</param>
-    public IndexedDictionary(int capacity) {
-        _dict = new Dictionary<TKey, TValue>(capacity);
-        _ordering = new List<TKey>(capacity);
-    }
-    /// <summary>
-    /// Initializes a new instance of the <see cref="IndexedDictionary{TKey,TValue}"/>
-    /// that contains elements copied from the specified System.Collections.Generic.IDictionary`2
-    /// and uses the specified System.Collections.Generic.IEqualityComparer`1.
-    /// </summary>
-    /// <param name="dictionary">The System.Collections.Generic.IDictionary`2 whose elements are copied to the new SquidLib.SquidMath.IndexedDictionary.</param>
-    /// <param name="comparer">The System.Collections.Generic.IEqualityComparer`1 implementation to use when comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1 for the type of the key.</param>
-    public IndexedDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) {
-        _dict = new Dictionary<TKey, TValue>(dictionary, comparer);
-        _ordering = new List<TKey>(_dict.Keys);
-    }
-    /// <summary>
-    /// Initializes a new instance of the <see cref="IndexedDictionary{TKey,TValue}"/>
-    /// that is empty, has the specified initial capacity, and uses the specified System.Collections.Generic.IEqualityComparer`1.
-    /// </summary>
-    /// <param name="capacity">The initial number of elements that the SquidLib.SquidMath.IndexedDictionary can contain.</param>
-    /// <param name="comparer">The System.Collections.Generic.IEqualityComparer`1 implementation to use when comparing keys, or null to use the default System.Collections.Generic.EqualityComparer`1 for the type of the key.</param>
-    public IndexedDictionary(int capacity, IEqualityComparer<TKey> comparer) {
-        _dict = new Dictionary<TKey, TValue>(capacity, comparer);
-        _ordering = new List<TKey>(capacity);
-    }
 
     public TValue this[TKey key] {
-        get => _dict[key];
-        set {
+	    get => _dict[key];
+	    set {
             if (!_dict.ContainsKey(key))
                 _ordering.Add(key);
             _dict[key] = value;
@@ -128,7 +78,7 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
     /// <param name="arrayIndex">The first index in array to insert this into.</param>
     public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => ((IDictionary<TKey, TValue>)_dict).CopyTo(array, arrayIndex);
 
-    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => new Enumerator(_dict, _ordering);
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => new KeyValuePairEnumerator(_dict, _ordering);
 
     public bool Remove(TKey key) {
         if (_dict.Remove(key)) {
@@ -146,15 +96,16 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
         return false;
     }
 
-    public bool TryGetValue(TKey key, out TValue value) => _dict.TryGetValue(key, out value);
+    public bool TryGetValue(TKey key, out TValue value) => _dict.TryGetValue(key, out value!);
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     public override bool Equals(object? obj) => Equals(obj as IndexedDictionary<TKey, TValue>);
 
-    public bool Equals(IndexedDictionary<TKey, TValue>? other) => other != null &&
-                                                                  EqualityComparer<Dictionary<TKey, TValue>>.Default.Equals(_dict, other._dict) &&
-                                                                  EqualityComparer<List<TKey>>.Default.Equals(_ordering, other._ordering);
+    public bool Equals(IndexedDictionary<TKey, TValue>? other) =>
+	    other != null! &&
+        EqualityComparer<Dictionary<TKey, TValue>>.Default.Equals(_dict, other._dict) &&
+        EqualityComparer<List<TKey>>.Default.Equals(_ordering, other._ordering);
 
     public override int GetHashCode() {
         var hashCode = -392379326;
@@ -172,25 +123,20 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
             _items = items ?? throw new ArgumentNullException(nameof(items));
         }
 
-        public Enumerator GetEnumerator() => new(_dictionary, _items);
+        public ValueEnumerator GetEnumerator() => new(_dictionary, _items);
 
         public void CopyTo(TValue[] array, int index) {
-            if (array == null) {
-                throw new ArgumentNullException(nameof(array));
-            }
+			ArgumentNullException.ThrowIfNull(array);
 
-            if ((uint)index > array.Length) {
-                throw new IndexOutOfRangeException("The index is not within the range for the array.");
-            }
+			if ((uint)index > array.Length)
+	            throw new IndexOutOfRangeException("The index is not within the range for the array.");
 
             var count = _items.Count;
-            if (array.Length - index < count) {
-                throw new ArgumentException("The given array is too small for the copied IndexedDictionary.");
-            }
+            if (array.Length - index < count)
+	            throw new ArgumentException("The given array is too small for the copied IndexedDictionary.");
 
-            for (var i = 0; i < count; i++) {
-                array[index++] = _dictionary[_items[i]];
-            }
+            for (var i = 0; i < count; i++)
+	            array[index++] = _dictionary[_items[i]];
         }
 
         public int Count => _dictionary.Count;
@@ -209,13 +155,14 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
             => _dictionary.ContainsValue(item);
 
         IEnumerator<TValue> IEnumerable<TValue>.GetEnumerator()
-            => new Enumerator(_dictionary, _items);
+            => new ValueEnumerator(_dictionary, _items);
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         void ICollection.CopyTo(Array array, int index) {
-            if (array is null) throw new ArgumentNullException(nameof(array));
-            if (array.Rank != 1) throw new ArgumentException("Multidimensional arrays are not supported for this CopyTo() operation.");
+			ArgumentNullException.ThrowIfNull(array);
+			if (array.Rank != 1)
+				throw new ArgumentException("Multidimensional arrays are not supported for this CopyTo() operation.");
             if (array.GetLowerBound(0) != 0)
                 throw new ArgumentException("The array must not have a lower bound other than 0.");
             if ((uint)index > (uint)array.Length)
@@ -223,21 +170,19 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
             if (array.Length - index < _dictionary.Count)
                 throw new ArgumentException("The given array is too small for the copied IndexedDictionary.");
 
-            if (array is TValue[] values) {
-                CopyTo(values, index);
-            } else {
+            if (array is TValue[] values)
+	            CopyTo(values, index);
+            else
+            {
                 var objects = array as object[];
-                if (objects is null) {
-                    throw new ArgumentException("The given array has an invalid type.");
-                }
+                if (objects is null)
+	                throw new ArgumentException("The given array has an invalid type.");
 
                 var count = _items.Count;
                 try
                 {
                     for (var i = 0; i < count; i++)
-                    {
-                        objects[index++] = _dictionary[_items[i]];
-                    }
+	                    objects[index++] = _dictionary[_items[i]]!;
                 }
                 catch (ArrayTypeMismatchException)
                 {
@@ -250,17 +195,17 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
 
         object ICollection.SyncRoot => ((ICollection)_dictionary).SyncRoot;
 
-        public struct Enumerator : IEnumerator<TValue>
+        public struct ValueEnumerator : IEnumerator<TValue>
         {
             private readonly Dictionary<TKey, TValue> _dictionary;
             private readonly List<TKey> _items;
             private int _index;
 
-            internal Enumerator(Dictionary<TKey, TValue> dictionary, List<TKey> items) {
+            internal ValueEnumerator(Dictionary<TKey, TValue> dictionary, List<TKey> items) {
                 _dictionary = dictionary;
                 _items = items;
                 _index = 0;
-                Current = default;
+                Current = default!;
             }
 
             public void Dispose() {
@@ -272,7 +217,7 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
                     return true;
                 }
                 _index = _items.Count;
-                Current = default;
+                Current = default!;
                 return false;
             }
 
@@ -280,44 +225,48 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
 
             object IEnumerator.Current {
                 get {
-                    if (_index <= 0 || (_index == _items.Count)) {
-                        throw new InvalidOperationException("Cannot get current item if the Enumerator hasn't started or has ended.");
-                    }
+                    if (_index <= 0 || (_index == _items.Count))
+	                    throw new InvalidOperationException("Cannot get current item if the Enumerator hasn't started or has ended.");
 
-                    return Current;
+                    return Current!;
                 }
             }
 
             void IEnumerator.Reset() {
                 _index = 0;
-                Current = default;
+                Current = default!;
             }
         }
     }
 
-    public static bool operator ==(IndexedDictionary<TKey, TValue> left, IndexedDictionary<TKey, TValue> right) =>
+    public static bool operator ==(IndexedDictionary<TKey, TValue>? left, IndexedDictionary<TKey, TValue>? right) =>
 	    EqualityComparer<IndexedDictionary<TKey, TValue>>.Default.Equals(left, right);
 
-    public static bool operator !=(IndexedDictionary<TKey, TValue> left, IndexedDictionary<TKey, TValue> right) =>
+    public static bool operator !=(IndexedDictionary<TKey, TValue>? left, IndexedDictionary<TKey, TValue> right) =>
 	    !(left == right);
 
-    public struct Enumerator : IEnumerator<KeyValuePair<TKey, TValue>>, IEnumerator {
+    public struct KeyValuePairEnumerator : IEnumerator<KeyValuePair<TKey, TValue>>
+    {
         private readonly Dictionary<TKey, TValue> _dictionary;
         private readonly List<TKey> _items;
         private int _index;
 
-        internal Enumerator(Dictionary<TKey, TValue> dictionary, List<TKey> items) {
+        internal KeyValuePairEnumerator(Dictionary<TKey, TValue> dictionary, List<TKey> items)
+        {
             _dictionary = dictionary;
             _items = items;
             _index = 0;
             Current = default;
         }
 
-        public void Dispose() {
+        public void Dispose()
+        {
         }
 
-        public bool MoveNext() {
-            if (_index < _items.Count) {
+        public bool MoveNext()
+        {
+            if (_index < _items.Count)
+            {
                 Current = new KeyValuePair<TKey, TValue>(_items[_index], _dictionary[_items[_index++]]);
                 return true;
             }
@@ -328,11 +277,12 @@ public class IndexedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEquat
 
         public KeyValuePair<TKey, TValue> Current { get; private set; }
 
-        object IEnumerator.Current {
-            get {
-                if (_index <= 0 || (_index == _items.Count)) {
-                    throw new InvalidOperationException("Cannot get current item if the Enumerator hasn't started or has ended.");
-                }
+        object IEnumerator.Current
+        {
+            get
+            {
+                if (_index <= 0 || _index == _items.Count)
+	                throw new InvalidOperationException("Cannot get current item if the Enumerator hasn't started or has ended.");
 
                 return Current;
             }
